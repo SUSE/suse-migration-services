b65d128f101f0f478ebb08f16b1726c82efeae42
Marcus Schäfer <ms@suse.de>
2018-10-10 15:43:09 +0200

    Added command and path helper classes

---
+++ suse_migration_services/exceptions.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2018 SUSE Linux LLC.  All rights reserved.
+#
+# This file is part of suse-migration-services.
+#
+# suse-migration-services is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# suse-migration-services is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with suse-migration-services. If not, see <http://www.gnu.org/licenses/>
+#
+
+
+class DistMigrationException(Exception):
+    """
+    Base class to handle all known exceptions.
+
+    Specific exceptions are implemented as sub classes of DistMigrationException
+
+    :param string message: Exception message text
+    """
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return format(self.message)
+
+
+class DistMigrationCommandException(DistMigrationException):
+    """
+    Exception raised if an external command called via a Command
+    instance has returned with an exit code != 0 or could not
+    be called at all.
+    """
+
+
+class DistMigrationCommandNotFoundException(DistMigrationException):
+    """
+    Exception raised if any executable command cannot be found in
+    the evironment PATH variable.
+    """
