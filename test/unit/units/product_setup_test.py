49de8aa0fca632f34638501fbc50fb8bbac1cc2c
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2019-01-17 13:46:43 +0000

    Detection of baseproduct
    Find the .prod files inside /etc/products.d
    which does not contain a flavor tag.
    
    Remove the target registration in that .prod file.

--- test/unit/units/product_setup_test.py
+++ test/unit/units/product_setup_test.py
@@ -4,24 +4,57 @@ from unittest.mock import (
 )
 from pytest import raises
 
-from suse_migration_services.units.product_setup import main
+from suse_migration_services.units.product_setup import (
+    main, get_baseproduct, delete_target_registration
+)
 from suse_migration_services.exceptions import (
     DistMigrationProductSetupException
 )
 
 
 class TestProductSetup(object):
+    @patch('suse_migration_services.units.product_setup.ElementTree.parse')
+    @patch('suse_migration_services.logger.log.warning')
+    def test_get_baseproduct_raises(
+        self, mock_warning, mock_ElementTree_parse
+    ):
+        mock_ElementTree_parse.side_effect = Exception
+        get_baseproduct('../data/etc/products.d/')
+        assert mock_warning.call_count == 2
+
+    @patch('suse_migration_services.units.product_setup.ElementTree')
+    @patch('suse_migration_services.logger.log.error')
+    def test_delete_target_registration_raises(
+        self, mock_error, mock_ElementTree
+    ):
+        mock_ElementTree.side_effect = Exception
+        delete_target_registration('../data/etc/products.d')
+        mock_error.assert_called_once()
+
+    @patch('suse_migration_services.defaults.Defaults.get_system_root_path')
     @patch('suse_migration_services.logger.log.error')
     @patch('suse_migration_services.logger.log.info')
-    @patch('os.path.exists')
-    def test_main_raises_on_product_setup(
-        self, mock_os_path_exists, mock_info, mock_error
+    def test_main_raises_on_product_setup_flavors(
+        self, mock_info, mock_error,
+        mock_get_system_root_path
     ):
-        mock_os_path_exists.side_effect = Exception
+        mock_get_system_root_path.return_value = '../data/bad_products/too_many'
         with raises(DistMigrationProductSetupException):
             main()
-            assert mock_info.called
-            assert mock_error.called
+        assert mock_error.called
+
+    @patch('suse_migration_services.defaults.Defaults.get_system_root_path')
+    @patch('suse_migration_services.logger.log.error')
+    @patch('suse_migration_services.logger.log.info')
+    def test_main_raises_on_product_setup_non_flavor(
+        self, mock_info, mock_error,
+        mock_get_system_root_path
+    ):
+        mock_get_system_root_path.return_value = '../data/bad_products/none'
+        with raises(DistMigrationProductSetupException):
+            main()
+        mock_error.assert_called_with('Base Product update failed with: '
+                                      'There is no baseproduct')
 
     @patch('suse_migration_services.units.product_setup.ElementTree')
     @patch('suse_migration_services.defaults.Defaults.get_system_root_path')
@@ -46,7 +79,7 @@ class TestProductSetup(object):
         )
         assert xml.findall('register/target') == []
         xml.write.assert_called_once_with(
-            '../data/etc/products.d/baseproduct',
+            '../data/etc/products.d/SLES.prod',
             encoding='UTF-8', xml_declaration=True
         )
         assert mock_info.called
