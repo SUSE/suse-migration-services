58bc2bbca24865110e72c3be9e2e9fa30447f4b4
Robert Schweikert <rjschwei@suse.com>
2019-05-29 18:08:19 -0400

    Cosmetic changes to handle error codes from zypper
    Make it more explicit to the reader that we treat specific
    values from zypper as errors. Positive side effect is that
    if the list gets longer the condition does not need to
    change

--- suse_migration_services/units/migrate.py
+++ suse_migration_services/units/migrate.py
@@ -116,14 +116,16 @@ def zypper_has_failed(returncode):
 
     :rtype: boolean
     """
+    error_codes = [
+        104,  # ZYPPER_EXIT_INF_CAP_NOT_FOUND
+        105,  # ZYPPER_EXIT_ON_SIGNAL
+        106,  # ZYPPER_EXIT_INF_REPOS_SKIPPED
+    ]
+
     if returncode == 0:
         # All is good
         return False
-    elif returncode == 104 or returncode == 105 or returncode == 106:
-        # Treat the following exit codes as error
-        # 104 - ZYPPER_EXIT_INF_CAP_NOT_FOUND
-        # 105 - ZYPPER_EXIT_ON_SIGNAL
-        # 106 - ZYPPER_EXIT_INF_REPOS_SKIPPED
+    elif returncode in error_codes:
         return True
     elif returncode >= 100:
         # Treat all other 100 codes as non error codes
