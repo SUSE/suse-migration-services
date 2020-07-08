f994384851ccb25d2f55a1ce3658396b351e8027
Marcus Schäfer <ms@suse.de>
2020-07-08 16:21:12 +0200

    Fixed reading of grub cmdline options
    If the grub setup uses linuxefi or a $linux variable
    the regular expression to extract the kernel cmdline
    failed. Related to bsc#1173532

--- suse_migration_services/units/kernel_load.py
+++ suse_migration_services/units/kernel_load.py
@@ -68,7 +68,9 @@ def main():
             ]
         )
     except Exception as issue:
-        log.error('Kernel load service raised exception: {0}', format(issue))
+        log.error(
+            'Kernel load service raised exception: {0}'.format(issue)
+        )
         raise DistMigrationKernelRebootException(
             'Failed to load kernel/initrd into memory: {0}'.format(
                 issue
@@ -94,7 +96,7 @@ def _get_cmdline(kernel_name):
         )
     with open(grub_config_file_path) as grub_config_file:
         grub_content = grub_config_file.read()
-    pattern = r'(?<=linux)[ \t]+{0}([{1}|boot/{1}].*)'.format(
+    pattern = r'(?:linux|linuxefi)[ \t]+{0}([{1}|boot/{1}].*)'.format(
         os.sep, kernel_name
     )
     cmd_line = re.findall(pattern, grub_content)[0]
--- test/unit/units/kernel_load_test.py
+++ test/unit/units/kernel_load_test.py
@@ -1,9 +1,12 @@
 import io
 import os
-from unittest.mock import (
+import logging
+from mock import (
     patch, call, MagicMock
 )
-from pytest import raises
+from pytest import (
+    raises, fixture
+)
 from suse_migration_services.defaults import Defaults
 from suse_migration_services.units.kernel_load import (
     main, _get_cmdline
@@ -14,20 +17,69 @@ from suse_migration_services.exceptions import (
 
 
 class TestKernelLoad(object):
+    @fixture(autouse=True)
+    def inject_fixtures(self, caplog):
+        self._caplog = caplog
+
     @patch('suse_migration_services.logger.Logger.setup')
     @patch('os.path.exists')
     def test_get_cmd_line_grub_cfg_not_present(
         self, mock_os_path_exists, mock_logger_setup
     ):
         mock_os_path_exists.return_value = False
-        with raises(DistMigrationKernelRebootException):
-            _get_cmdline(Defaults.get_grub_config_file())
+        with self._caplog.at_level(logging.ERROR):
+            with raises(DistMigrationKernelRebootException):
+                _get_cmdline(Defaults.get_grub_config_file())
 
     @patch('suse_migration_services.logger.Logger.setup')
     @patch('os.path.exists')
     def test_get_cmd_line(self, mock_path_exists, mock_logger_setup):
         mock_path_exists.return_value = True
         with open('../data/fake_grub.cfg') as fake_grub:
+            fake_grub_data = fake_grub.read()
+        with patch('builtins.open', create=True) as mock_open:
+            mock_open.return_value = MagicMock(spec=io.IOBase)
+            file_handle = mock_open.return_value.__enter__.return_value
+            file_handle.read.return_value = fake_grub_data
+            grub_cmd_content = \
+                'root=UUID=ec7aaf92-30ea-4c07-991a-4700177ce1b8 ' + \
+                'splash root=UUID=ec7aaf92-30ea-4c07-991a-4700177ce1b8 rw'
+            result = _get_cmdline(
+                os.path.basename(Defaults.get_target_kernel())
+            )
+            mock_open.assert_called_once_with(
+                '/system-root/boot/grub2/grub.cfg'
+            )
+            assert result == grub_cmd_content
+
+    @patch('suse_migration_services.logger.Logger.setup')
+    @patch('os.path.exists')
+    def test_get_cmd_line_linuxefi(self, mock_path_exists, mock_logger_setup):
+        mock_path_exists.return_value = True
+        with open('../data/fake_grub_linuxefi.cfg') as fake_grub:
+            fake_grub_data = fake_grub.read()
+        with patch('builtins.open', create=True) as mock_open:
+            mock_open.return_value = MagicMock(spec=io.IOBase)
+            file_handle = mock_open.return_value.__enter__.return_value
+            file_handle.read.return_value = fake_grub_data
+            grub_cmd_content = \
+                'root=UUID=ec7aaf92-30ea-4c07-991a-4700177ce1b8 ' + \
+                'splash root=UUID=ec7aaf92-30ea-4c07-991a-4700177ce1b8 rw'
+            result = _get_cmdline(
+                os.path.basename(Defaults.get_target_kernel())
+            )
+            mock_open.assert_called_once_with(
+                '/system-root/boot/grub2/grub.cfg'
+            )
+            assert result == grub_cmd_content
+
+    @patch('suse_migration_services.logger.Logger.setup')
+    @patch('os.path.exists')
+    def test_get_cmd_line_linux_variable(
+        self, mock_path_exists, mock_logger_setup
+    ):
+        mock_path_exists.return_value = True
+        with open('../data/fake_grub_linux_var.cfg') as fake_grub:
             fake_grub_data = fake_grub.read()
         with patch('builtins.open', create=True) as mock_open:
             mock_open.return_value = MagicMock(spec=io.IOBase)
@@ -84,10 +136,11 @@ class TestKernelLoad(object):
             '../data/migration-config.yml'
         mock_Command_run.side_effect = [
             None,
-            Exception
+            Exception('error')
         ]
-        with raises(DistMigrationKernelRebootException):
-            main()
+        with self._caplog.at_level(logging.ERROR):
+            with raises(DistMigrationKernelRebootException):
+                main()
         assert mock_Command_run.call_args_list == [
             call(
                 ['mkdir', '-p', '/var/tmp/kexec']
