491312bc04942cffdf6eded1f06f4714dc96c598
Frederic Crozat <fcrozat@suse.com>
2025-01-14 07:30:08 +0100

    initial sles16 migration image

--- Makefile
+++ Makefile
@@ -62,6 +62,40 @@ sle15_activation: check
            exit 1;\
 	fi
 
+sle16_activation: check
+	rm -f dist/*
+	# update rpm changelog using reference file
+	helper/update_changelog.py \
+		--since package/suse-migration-sle16-activation.changes.ref \
+		--from package/suse-migration-sle16-activation-spec-template \
+		--from grub.d --utc > \
+		dist/suse-migration-sle16-activation.changes
+	helper/update_changelog.py \
+		--file package/suse-migration-sle16-activation.changes >> \
+		dist/suse-migration-sle16-activation.changes
+	# update package version in spec file
+	cat package/suse-migration-sle16-activation-spec-template \
+		| sed -e s'@%%VERSION@${version}@' \
+		> dist/suse-migration-sle16-activation.spec
+	# include grub.d dir in MANIFEST
+	sed -ie s'@prune grub.d@graft grub.d@' MANIFEST.in
+	rm MANIFEST.ine
+	python3 setup.py sdist
+	# create tarball with grub.d + suse-migration-services
+	mv dist/suse_migration_services-*.tar.gz  \
+		dist/suse-migration-sle16-activation.tar.gz
+	# restore MANIFEST.ini
+	git checkout MANIFEST.in
+	# check MANIFEST.in has prune grub.d
+	# raise error if not
+	$(eval current_value = $(shell grep "prune grub.d" MANIFEST.in))
+	$(eval expected_value = "prune grub.d")
+
+	@if [ "$(current_value)" != $(expected_value) ]; then\
+	   echo "MANIFEST.in does not have '$(expected_value)'";\
+           exit 1;\
+	fi
+
 .PHONY: test
 test:
 	tox
---
+++ image/generic/sle16/config.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#================
+# FILE          : config.sh
+#----------------
+# PROJECT       : KIWI - Image System
+# COPYRIGHT     : (c) 2023 SUSE Software Solutions Germany GmbH
+#               :
+# AUTHORS       : Marcus Schaefer <ms@suse.de>
+#               : Keith Berger <kberger@suse.com>
+#               :
+# BELONGS TO    : Operating System images
+#               :
+# DESCRIPTION   : configuration script for SUSE based
+#               : operating systems
+#----------------
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Setup baseproduct link
+#--------------------------------------
+suseSetupProduct
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
+
+#======================================
+# Deactivate services
+#--------------------------------------
+# cloud-regionsrv-client packages(s) activates that service
+# on install. For migration we don't need this service.
+# The installation of that packages and its relatives happens
+# to satisfy the dependencies on the zypper plugin side but
+# not for actually registering a guest to the public cloud
+# infrastructure. We expect that step to be already done
+suseRemoveService guestregister
+
+# disable and mask lvmetad
+systemctl disable lvm2-lvmetad.socket
+systemctl disable lvm2-lvmetad.service
+systemctl mask lvm2-lvmetad.socket
+systemctl mask lvm2-lvmetad.service
+
+#======================================
+# Activate services
+#--------------------------------------
+suseInsertService sshd
+suseInsertService haveged
+
+#======================================
+# Activate migration services
+#--------------------------------------
+suseInsertService suse-migration-mount-system
+suseInsertService suse-migration-post-mount-system
+suseInsertService suse-migration-ssh-keys
+suseInsertService suse-migration-pre-checks
+suseInsertService suse-migration-setup-host-network
+suseInsertService suse-migration-prepare
+suseInsertService suse-migration
+suseInsertService suse-migration-console-log
+suseInsertService suse-migration-grub-setup
+suseInsertService suse-migration-update-bootloader
+suseInsertService suse-migration-product-setup
+suseInsertService suse-migration-regenerate-initrd
+suseInsertService suse-migration-kernel-load
+suseInsertService suse-migration-reboot
+suseInsertService NetworkManager
+mkdir -p /etc/sysconfig/network/providers
+
+# Disable password based login via ssh
+sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' \
+    /etc/ssh/sshd_config
+
+# Remove the password for root and migration user
+# Note the string matches the password set in the config file
+# FIXME: The line below must be active on production
+# sed -i 's/$1$wYJUgpM5$RXMMeASDc035eX.NbYWFl0/*/' /etc/shadow
+
+# Setup ownership for migration user data
+chown -R migration:users /home/migration
