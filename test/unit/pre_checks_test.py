3d6e64eac85475170f356e9450f3c8011510185b
Frederic Crozat <fcrozat@suse.com>
2025-06-13 16:27:13 +0200

    fixup! ensure SLES16 is also detected in SCC precheck

--- test/unit/pre_checks_test.py
+++ test/unit/pre_checks_test.py
@@ -701,6 +701,26 @@ class TestPreChecks():
             'arch': 'x86_64'
         }
 
+    @patch('platform.machine')
+    @patch('os.path.isfile')
+    @patch.object(Command, 'run')
+    def test_check_scc_migration_default_target_sles16(
+        self, mock_Command_run, mock_os_path_isfile,
+        mock_platform_machine, mock_os_geteuid, mock_log
+    ):
+        mock_platform_machine.return_value = 'x86_64'
+        sles15_migration_not_found = Mock()
+        sles15_migration_not_found.returncode = 1 # SLES15-Migration not found
+        sles16_migration_found = Mock()
+        sles16_migration_found.returncode = 0 # SLES16-Migration found
+        mock_Command_run.side_effect = [sles15_migration_not_found, sles16_migration_found]
+        mock_os_path_isfile.return_value = False # No /etc/sle-migration-service.yml
+        assert check_scc.get_migration_target() == {
+            'identifier': 'SLES',
+            'version': '16.0',
+            'arch': 'x86_64'
+        }
+
     def test_privileges(self, mock_os_geteuid, mock_log):
         with self._caplog.at_level(logging.ERROR):
             check_pre_checks.main()
