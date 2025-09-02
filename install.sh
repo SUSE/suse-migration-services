6d10f9b8ca77b56f16ff224ace5757c9f1e8a7db
Robert Frohl <rfrohl@suse.com>
2025-09-02 13:46:00 +0200

    Draft: lsm check for AppArmor -> SELinux
    Needs removal
    - install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+#cp suse_migration_services/prechecks/pre_checks.py /usr/lib/python3.6/site-packages/suse_migration_services/prechecks/pre_checks.py 
+cp suse_migration_services/prechecks/lsm.py  /usr/lib/python3.6/site-packages/suse_migration_services/prechecks/
