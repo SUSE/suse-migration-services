19fda02d257fa6e9994f0d7981bc968325e3942e
Marcus Schäfer <marcus.schaefer@suse.com>
2025-09-12 15:46:11 +0200

    Update SLE16 migration container
    Match with the changes done in the SLE16 live image

--- container/sle16/config.sh
+++ container/sle16/config.sh
@@ -16,6 +16,9 @@ EOF
 #--------------------------------------
 mkdir -p /system-root
 
+# needed for network setup migration
+mkdir -p /etc/sysconfig/network/providers
+
 #======================================
 # Activate migration services
 #--------------------------------------
@@ -23,7 +26,26 @@ for service in \
     suse-migration-container.service \
     suse-migration-container-prepare.service \
     suse-migration-container-product-setup.service \
-    suse-migration-container-reboot.service
+    suse-migration-container-reboot.service \
+    suse-migration-grub-setup.service \
+    suse-migration-apparmor-selinux.service \
+    suse-migration-container-btrfs-snapshot-pre-migration.service \
+    suse-migration-btrfs-snapshot-post-migration.service \
+    suse-migration-regenerate-initrd.service \
+    suse-migration-wicked-networkmanager.service \
+    suse-migration-ha.service
 do
     systemctl enable "${service}"
 done
+
+#======================================
+# Udev rules
+#--------------------------------------
+if [ "$(arch)" = "s390x" ]; then
+    cat <<EOF >> /etc/migration-config.yml
+preserve:
+  rules:
+    - /etc/udev/rules.d/*qeth*.rules
+    - /etc/udev/rules.d/*-cio-ignore*.rules
+EOF
+fi
