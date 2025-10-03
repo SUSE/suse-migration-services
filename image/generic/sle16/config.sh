6f55e04a6e85ed1eec600ffec0b44a16377dac79
Frederic Crozat <fcrozat@suse.com>
2025-10-03 17:29:02 +0200

    shrink migration image
    remove a lot of things in the image, based on the work done on Agama
    Live ISO and Tumbleweed Live ISO

--- image/generic/sle16/config.sh
+++ image/generic/sle16/config.sh
@@ -88,3 +88,68 @@ preserve:
     - /etc/udev/rules.d/*-cio-ignore*.rules
 EOF
 fi
+
+# optimize iso size (based from Agama live image)
+
+# Clean-up logs
+rm /var/log/zypper.log /var/log/zypp/history
+
+du -h -s /usr/{share,lib}/locale/
+
+# keep the en_US translations
+ls -1 -I en -I en_US -I "C.utf8" /usr/share/locale/ | xargs -I% sh -c "echo 'Removing translations %...' && rm -rf /usr/share/locale/%"
+
+# delete locale definitions for unsupported languages (explicitly keep the C and en_US locales)
+ls -1 -I "en_US.utf8" -I "C.utf8" /usr/lib/locale/ | xargs -I% sh -c "echo 'Removing locale %...' && rm -rf /usr/lib/locale/%"
+
+# delete unused translations (MO files)
+for t in Linux-PAM e2fsprogs gawk gettext-runtime grub2 iputils kbd libgpg-error mit-krb5 p11-kit pam-config rpm shadow.mo sudo sudoers xfsprogs zypp zypper; do
+  rm -f /usr/share/locale/*/LC_MESSAGES/$t.mo
+done
+du -h -s /usr/{share,lib}/locale/
+
+# remove documentation
+du -h -s /usr/share/doc/packages/
+rm -rf /usr/share/doc/packages/*
+# remove man pages
+du -h -s /usr/share/man
+rm -rf /usr/share/man/*
+
+# remove unused SUSEConnect libzypp plugins
+rm -f /usr/lib/zypper/commands/zypper-search-packages
+
+## removing drivers and firmware makes the Live ISO smaller
+
+# driver and firmware cleanup
+du -h -s /lib/modules /lib/firmware
+
+# remove unneeded drivers (disabled for now)
+#image-janitor driver-cleanup --delete --config-files /usr/share/image-janitor/modules.list*
+
+# remove the unused firmware (not referenced by kernel drivers)
+image-janitor -v fw-cleanup --delete 
+# remove the tool, not needed anymore
+rpm -e image-janitor
+du -h -s /lib/modules /lib/firmware
+
+################################################################################
+# The rest of the file was copied from the openSUSE Tumbleweed Live ISO
+# https://build.opensuse.org/projects/openSUSE:Factory:Live/packages/livecd-tumbleweed-kde/files/config.sh?expand=1
+#
+
+# Stronger compression for the initrd
+echo 'compress="xz -9 --check=crc32 --memlimit-compress=50%"' >> /etc/dracut.conf.d/less-storage.conf
+
+# Decompress kernel modules, better for squashfs (boo#1192457)
+find /lib/modules/*/kernel -name '*.ko.xz' -exec xz -d {} +
+find /lib/modules/*/kernel -name '*.ko.zst' -exec zstd --rm -d {} +
+for moddir in /lib/modules/*; do
+  depmod "$(basename "$moddir")"
+done
+
+# Not needed, boo#1166406
+rm -f /boot/vmlinux*.[gx]z
+rm -f /lib/modules/*/vmlinux*.[gx]z
+
+# Remove generated files (boo#1098535)
+rm -rf /var/cache/zypp/* /var/lib/zypp/AnonymousUniqueId /var/lib/systemd/random-seed
