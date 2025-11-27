SUSE/suse-migration-services

commit 15549f619f0cde97bad341166ca4ae01c2a3d799

Add DropComponents class
Implement DropComponents class to allow for removal of
packages, files and directories. Every service class that
has the need to perform this task needs to inherit from
DropComponents. Along with the new class also all service
units are turned into classes for consistency and to allow
the inheritance model for eventually further extensions
to the service units. This is related to Issue #421

suse_migration_services/units/mount_system.py
@@ -33,242 +33,243 @@ from suse_migration_services.exceptions import (
 )
 
 
-def main():
-    """
-    DistMigration mount system to upgrade
+class MountSystem:
+    def __init__(self):
+        """
+        DistMigration mount system to upgrade
 
-    Searches on all partitions for a fstab file. The first
-    fstab file found is used as the system to upgrade.
-    Filesystems relevant for an upgrade process are read from
-    that fstab in order and mounted such that the system rootfs
-    is available for a zypper based migration process.
-    """
-    Logger.setup()
-    log = logging.getLogger(Defaults.get_migration_log_name())
-    root_path = Defaults.get_system_root_path()
-    Path.create(root_path)
-    log.info('Running mount system service')
+        Searches on all partitions for a fstab file. The first
+        fstab file found is used as the system to upgrade.
+        Filesystems relevant for an upgrade process are read from
+        that fstab in order and mounted such that the system rootfs
+        is available for a zypper based migration process.
+        """
+        Logger.setup()
+        self.log = logging.getLogger(Defaults.get_migration_log_name())
+        self.root_path = Defaults.get_system_root_path()
 
-    if is_mounted(root_path):
-        # root_path is already a mount point, better not continue
-        # The condition is not handled as an error because the
-        # existing mount point under this service created root_path
-        # is considered to represent the system to upgrade and
-        # not something else. Thus if already mounted, let's use
-        # what is there.
-        return
+    def perform(self):
+        Path.create(self.root_path)
+        self.log.info('Running mount system service')
 
-    log.info('Mount system service: {0} is mounted'.format(root_path))
-    # Check if booted via loopback grub
-    isoscan_loop_mount = '/run/initramfs/isoscan'
-    if is_mounted(isoscan_loop_mount):
-        # The system to become migrated was booted via a grub
-        # loopback menuentry. This means the disk is blocked by
-        # that readonly loopback mount and needs to be
-        # remounted for read write access first
-        log.info(
-            'Mount system service: {0} is mounted'
-            .format(isoscan_loop_mount)
+        if self.is_mounted(self.root_path):
+            # root_path is already a mount point, better not continue
+            # The condition is not handled as an error because the
+            # existing mount point under this service created root_path
+            # is considered to represent the system to upgrade and
+            # not something else. Thus if already mounted, let's use
+            # what is there.
+            return
+
+        self.log.info('Mount system service: {0} is mounted'.format(
+            self.root_path)
         )
-        Command.run(
-            ['mount', '-o', 'remount,rw', isoscan_loop_mount]
-        )
-
-    activate_lvm()
-
-    mount_system(
-        root_path, read_system_fstab(root_path)
-    )
-
-    migration_config = MigrationConfig()
-    migration_config.update_migration_config_file()
-    log.info(
-        'Config file content:\n{content}\n'.format(
-            content=migration_config.get_migration_config_file_content()
-        )
-    )
-
-
-def get_uuid(device):
-    """
-    Retrieve UUID from block device
-    """
-    blkid_result = Command.run(
-        ['blkid', device, '-s', 'UUID', '-o', 'value'],
-        raise_on_error=False
-    )
-    return blkid_result.output.strip(os.linesep) if blkid_result else ''
-
-
-def activate_lvm():
-    log = logging.getLogger(Defaults.get_migration_log_name())
-    lsblk_call = Command.run(
-        ['lsblk', '-p', '-n', '-r', '-o', 'NAME,TYPE']
-    )
-    for entry in lsblk_call.output.split(os.linesep):
-        block_record = entry.split()
-        if len(block_record) >= 2:
-            block_type = block_record[1]
-            if block_type == 'lvm':
-                log.info(
-                    'LVM managed block device(s) found, activating LVM'
-                )
-                Command.run(['vgchange', '-a', 'y'])
-                return
-
-
-def get_target_root():
-    """
-    Read the migration_target information from the kernel cmdline
-    and provide the associated device name. The migration_target
-    information was placed to the cmdline by the DMS grub boot
-    menu entry of by the DMS run_migration kexec cmdline parameter
-    """
-    log = logging.getLogger(Defaults.get_migration_log_name())
-    try:
-        with open('/proc/cmdline') as cmdline_fd:
-            cmdline = cmdline_fd.read()
-        match = re.search(r'migration_target=([\w-]+)', cmdline)
-        if match:
-            migration_rootfs_uuid = match.group(1)
-            lsblk_call = Command.run(
-                ['lsblk', '-p', '-n', '-r', '-o', 'NAME,TYPE']
+        # Check if booted via loopback grub
+        isoscan_loop_mount = '/run/initramfs/isoscan'
+        if self.is_mounted(isoscan_loop_mount):
+            # The system to become migrated was booted via a grub
+            # loopback menuentry. This means the disk is blocked by
+            # that readonly loopback mount and needs to be
+            # remounted for read write access first
+            self.log.info(
+                'Mount system service: {0} is mounted'
+                .format(isoscan_loop_mount)
             )
-            considered_block_types = ['part', 'raid', 'lvm']
-            for entry in lsblk_call.output.split(os.linesep):
-                block_record = entry.split()
-                if len(block_record) >= 2:
-                    block_type = block_record[1]
-                    if block_type in considered_block_types:
-                        device = block_record[0]
-                        uuid = get_uuid(device)
-                        if uuid == migration_rootfs_uuid:
-                            return device
-        # nothing was found
-        raise DistMigrationSystemMountException(
-            'no match for migration_target= in cmdline'
-        )
-    except Exception as issue:
-        message = 'Failed to find target disk: {0}'.format(issue)
-        log.error(message)
-        raise DistMigrationSystemMountException(
-            message
-        )
-
-
-def read_system_fstab(root_path):
-    log = logging.getLogger(Defaults.get_migration_log_name())
-    log.info('Reading fstab from DMS selected target disk')
-    migration_target_root = get_target_root()
-    try:
-        log.info(
-            'Lookup for fstab on {0}'.format(migration_target_root)
-        )
-        Command.run(
-            ['mount', migration_target_root, root_path]
-        )
-        fstab_file = os.sep.join(
-            [root_path, 'etc', 'fstab']
-        )
-        if os.path.isfile(fstab_file):
-            log.info(
-                'Found {0} on {1}'.format(fstab_file, migration_target_root)
-            )
-            fstab = Fstab()
-            fstab.read(fstab_file)
-            return fstab
-        raise DistMigrationSystemNotFoundException(
-            'Could not find system with fstab on {0}'.format(
-                migration_target_root
-            )
-        )
-    except Exception as issue:
-        Command.run(
-            ['umount', root_path], raise_on_error=False
-        )
-        raise DistMigrationSystemNotFoundException(
-            'Reading fstab failed with: {}'.format(issue)
-        )
-
-
-def mount_system(root_path, fstab):
-    log = logging.getLogger(Defaults.get_migration_log_name())
-    log.info('Mount system in {0}'.format(root_path))
-    system_mount = Fstab()
-    explicit_mount_points = {
-        'devtmpfs': os.sep.join([root_path, 'dev']),
-        'proc': os.sep.join([root_path, 'proc']),
-        'sysfs': os.sep.join([root_path, 'sys'])
-    }
-    try:
-        for fstab_entry in fstab.get_devices():
-            mountpoint = ''.join(
-                [root_path, fstab_entry.mountpoint]
-            )
-            if mountpoint not in explicit_mount_points.values():
-                if fstab_entry.eligible_for_mount:
-                    log.info('Mounting {0}'.format(mountpoint))
-                    Command.run(
-                        [
-                            'mount', '-o', fstab_entry.options,
-                            fstab_entry.device, mountpoint
-                        ]
-                    )
-                system_mount.add_entry(
-                    fstab_entry.device, mountpoint, fstab_entry.fstype,
-                    fstab_entry.eligible_for_mount
-                )
-
-        log.info('Mounting kernel file systems inside {0}'.format(root_path))
-        for mount_type, mount_point in explicit_mount_points.items():
             Command.run(
-                ['mount', '-t', mount_type, mount_type, mount_point]
+                ['mount', '-o', 'remount,rw', isoscan_loop_mount]
             )
-            system_mount.add_entry(
-                mount_type, mount_point
+
+        self.activate_lvm()
+
+        self.mount_system(
+            self.root_path, self.read_system_fstab(self.root_path)
+        )
+
+        migration_config = MigrationConfig()
+        migration_config.update_migration_config_file()
+        self.log.info(
+            'Config file content:\n{content}\n'.format(
+                content=migration_config.get_migration_config_file_content()
             )
-        log.info(
-            'Bind mount subdirectories from /run inside chroot {0}'.format(
-                root_path
+        )
+
+    def get_uuid(self, device):
+        """
+        Retrieve UUID from block device
+        """
+        blkid_result = Command.run(
+            ['blkid', device, '-s', 'UUID', '-o', 'value'],
+            raise_on_error=False
+        )
+        return blkid_result.output.strip(os.linesep) if blkid_result else ''
+
+    def activate_lvm(self):
+        lsblk_call = Command.run(
+            ['lsblk', '-p', '-n', '-r', '-o', 'NAME,TYPE']
+        )
+        for entry in lsblk_call.output.split(os.linesep):
+            block_record = entry.split()
+            if len(block_record) >= 2:
+                block_type = block_record[1]
+                if block_type == 'lvm':
+                    self.log.info(
+                        'LVM managed block device(s) found, activating LVM'
+                    )
+                    Command.run(['vgchange', '-a', 'y'])
+                    return
+
+    def get_target_root(self):
+        """
+        Read the migration_target information from the kernel cmdline
+        and provide the associated device name. The migration_target
+        information was placed to the cmdline by the DMS grub boot
+        menu entry of by the DMS run_migration kexec cmdline parameter
+        """
+        try:
+            with open('/proc/cmdline') as cmdline_fd:
+                cmdline = cmdline_fd.read()
+            match = re.search(r'migration_target=([\w-]+)', cmdline)
+            if match:
+                migration_rootfs_uuid = match.group(1)
+                lsblk_call = Command.run(
+                    ['lsblk', '-p', '-n', '-r', '-o', 'NAME,TYPE']
+                )
+                considered_block_types = ['part', 'raid', 'lvm']
+                for entry in lsblk_call.output.split(os.linesep):
+                    block_record = entry.split()
+                    if len(block_record) >= 2:
+                        block_type = block_record[1]
+                        if block_type in considered_block_types:
+                            device = block_record[0]
+                            uuid = self.get_uuid(device)
+                            if uuid == migration_rootfs_uuid:
+                                return device
+            # nothing was found
+            raise DistMigrationSystemMountException(
+                'no match for migration_target= in cmdline'
             )
+        except Exception as issue:
+            message = 'Failed to find target disk: {0}'.format(issue)
+            self.log.error(message)
+            raise DistMigrationSystemMountException(
+                message
+            )
+
+    def read_system_fstab(self):
+        self.log.info('Reading fstab from DMS selected target disk')
+        migration_target_root = self.get_target_root()
+        try:
+            self.log.info(
+                'Lookup for fstab on {0}'.format(migration_target_root)
+            )
+            Command.run(
+                ['mount', migration_target_root, self.root_path]
+            )
+            fstab_file = os.sep.join(
+                [self.root_path, 'etc', 'fstab']
+            )
+            if os.path.isfile(fstab_file):
+                self.log.info(
+                    'Found {0} on {1}'.format(fstab_file, migration_target_root)
+                )
+                fstab = Fstab()
+                fstab.read(fstab_file)
+                return fstab
+            raise DistMigrationSystemNotFoundException(
+                'Could not find system with fstab on {0}'.format(
+                    migration_target_root
+                )
+            )
+        except Exception as issue:
+            Command.run(
+                ['umount', self.root_path], raise_on_error=False
+            )
+            raise DistMigrationSystemNotFoundException(
+                'Reading fstab failed with: {}'.format(issue)
+            )
+
+    def mount_system(self, fstab):
+        self.log.info('Mount system in {0}'.format(self.root_path))
+        system_mount = Fstab()
+        explicit_mount_points = {
+            'devtmpfs': os.sep.join([self.root_path, 'dev']),
+            'proc': os.sep.join([self.root_path, 'proc']),
+            'sysfs': os.sep.join([self.root_path, 'sys'])
+        }
+        try:
+            for fstab_entry in fstab.get_devices():
+                mountpoint = ''.join(
+                    [self.root_path, fstab_entry.mountpoint]
+                )
+                if mountpoint not in explicit_mount_points.values():
+                    if fstab_entry.eligible_for_mount:
+                        self.log.info('Mounting {0}'.format(mountpoint))
+                        Command.run(
+                            [
+                                'mount', '-o', fstab_entry.options,
+                                fstab_entry.device, mountpoint
+                            ]
+                        )
+                    system_mount.add_entry(
+                        fstab_entry.device, mountpoint, fstab_entry.fstype,
+                        fstab_entry.eligible_for_mount
+                    )
+
+            self.log.info(
+                'Mounting kernel file systems inside {0}'.format(self.root_path)
+            )
+            for mount_type, mount_point in explicit_mount_points.items():
+                Command.run(
+                    ['mount', '-t', mount_type, mount_type, mount_point]
+                )
+                system_mount.add_entry(
+                    mount_type, mount_point
+                )
+            self.log.info(
+                'Bind mount subdirectories from /run inside chroot {0}'.format(
+                    self.root_path
+                )
+            )
+            os.makedirs(
+                os.sep.join([self.root_path, 'run', 'NetworkManager']),
+                exist_ok=True
+            )
+            Command.run(
+                [
+                    'mount', '-o', 'bind', '/run/NetworkManager',
+                    os.sep.join([self.root_path, 'run', 'NetworkManager'])
+                ]
+            )
+            os.makedirs(
+                os.sep.join([self.root_path, 'run', 'netconfig']),
+                exist_ok=True
+            )
+            Command.run(
+                [
+                    'mount', '-o', 'bind', '/run/netconfig',
+                    os.sep.join([self.root_path, 'run', 'netconfig'])
+                ]
+            )
+        except Exception as issue:
+            self.log.error(
+                'Mounting system for upgrade failed with {0}'.format(issue)
+            )
+            raise DistMigrationSystemMountException(
+                'Mounting system for upgrade failed with {0}'.format(issue)
+            )
+        system_mount.export(
+            Defaults.get_system_mount_info_file()
         )
-        os.makedirs(
-            os.sep.join([root_path, 'run', 'NetworkManager']),
-            exist_ok=True
-        )
-        Command.run(
-            [
-                'mount', '-o', 'bind', '/run/NetworkManager',
-                os.sep.join([root_path, 'run', 'NetworkManager'])
-            ]
-        )
-        os.makedirs(
-            os.sep.join([root_path, 'run', 'netconfig']),
-            exist_ok=True
-        )
-        Command.run(
-            [
-                'mount', '-o', 'bind', '/run/netconfig',
-                os.sep.join([root_path, 'run', 'netconfig'])
-            ]
-        )
-    except Exception as issue:
-        log.error(
-            'Mounting system for upgrade failed with {0}'.format(issue)
-        )
-        raise DistMigrationSystemMountException(
-            'Mounting system for upgrade failed with {0}'.format(issue)
-        )
-    system_mount.export(
-        Defaults.get_system_mount_info_file()
-    )
+
+    def is_mounted(self, mount_point):
+        self.log.info('Checking {0} is mounted'.format(mount_point))
+        # a bind mount is neither a device nor an inode.
+        # Thus, ismount will return False for those, as
+        # it is very unlikely that a bind mount is used in fstab,
+        # that case is not handled
+        return os.path.ismount(mount_point)
 
 
-def is_mounted(mount_point):
-    log = logging.getLogger(Defaults.get_migration_log_name())
-    log.info('Checking {0} is mounted'.format(mount_point))
-    # a bind mount is neither a device nor an inode.
-    # Thus, ismount will return False for those, as
-    # it is very unlikely that a bind mount is used in fstab,
-    # that case is not handled
-    return os.path.ismount(mount_point)
+def main():
+    mount_os = MountSystem()
+    mount_os.perform()

test/unit/units/mount_system_test.py
@@ -6,8 +6,7 @@ from pytest import raises
 from collections import namedtuple
 
 from suse_migration_services.units.mount_system import (
-    main, mount_system, get_uuid, activate_lvm,
-    get_target_root, is_mounted, read_system_fstab
+    MountSystem, main
 )
 from suse_migration_services.migration_config import MigrationConfig
 from suse_migration_services.fstab import Fstab
@@ -19,9 +18,18 @@ from suse_migration_services.exceptions import (
 
 class TestMountSystem():
     @patch('suse_migration_services.logger.Logger.setup')
+    def setup(self, mock_Logger_setup):
+        self.mount_os = MountSystem()
+        self.mount_os.root_path = 'some'
+
+    @patch('suse_migration_services.logger.Logger.setup')
+    def setup_method(self, cls, mock_Logger_setup):
+        self.setup()
+
+    @patch('suse_migration_services.logger.Logger.setup')
     @patch('suse_migration_services.units.mount_system.Path.create')
-    @patch('suse_migration_services.units.mount_system.is_mounted')
-    @patch('suse_migration_services.units.mount_system.mount_system')
+    @patch('suse_migration_services.units.mount_system.MountSystem.is_mounted')
+    @patch('suse_migration_services.units.mount_system.MountSystem.mount_system')
     def test_main_root_already_mounted(
         self,
         mock_mount_system,
@@ -36,10 +44,10 @@ class TestMountSystem():
 
     @patch('suse_migration_services.logger.Logger.setup')
     @patch('suse_migration_services.units.mount_system.Path.create')
-    @patch('suse_migration_services.units.mount_system.is_mounted')
-    @patch('suse_migration_services.units.mount_system.mount_system')
-    @patch('suse_migration_services.units.mount_system.read_system_fstab')
-    @patch('suse_migration_services.units.mount_system.activate_lvm')
+    @patch('suse_migration_services.units.mount_system.MountSystem.is_mounted')
+    @patch('suse_migration_services.units.mount_system.MountSystem.mount_system')
+    @patch('suse_migration_services.units.mount_system.MountSystem.read_system_fstab')
+    @patch('suse_migration_services.units.mount_system.MountSystem.activate_lvm')
     @patch('suse_migration_services.command.Command.run')
     @patch.object(MigrationConfig, 'update_migration_config_file')
     def test_main_perform(
@@ -71,7 +79,7 @@ class TestMountSystem():
 
     @patch('suse_migration_services.command.Command.run')
     def test_get_uuid(self, mock_Command_run):
-        get_uuid('some')
+        self.mount_os.get_uuid('some')
         mock_Command_run.assert_called_once_with(
             ['blkid', 'some', '-s', 'UUID', '-o', 'value'],
             raise_on_error=False
@@ -87,7 +95,7 @@ class TestMountSystem():
             error='',
             returncode=0
         )
-        activate_lvm()
+        self.mount_os.activate_lvm()
         assert mock_Command_run.call_args_list == [
             call(['lsblk', '-p', '-n', '-r', '-o', 'NAME,TYPE']),
             call(['vgchange', '-a', 'y'])
@@ -100,10 +108,10 @@ class TestMountSystem():
             file_handle = mock_open.return_value.__enter__.return_value
             file_handle.read.return_value = 'foo'
             with raises(DistMigrationSystemMountException):
-                get_target_root()
+                self.mount_os.get_target_root()
 
     @patch('suse_migration_services.command.Command.run')
-    @patch('suse_migration_services.units.mount_system.get_uuid')
+    @patch('suse_migration_services.units.mount_system.MountSystem.get_uuid')
     def test_get_target_root_match_found(
         self,
         mock_get_uuid,
@@ -122,16 +130,16 @@ class TestMountSystem():
             mock_open.return_value = MagicMock(spec=io.IOBase)
             file_handle = mock_open.return_value.__enter__.return_value
             file_handle.read.return_value = 'migration_target=UUID'
-            assert get_target_root() == '/dev/sda4'
+            assert self.mount_os.get_target_root() == '/dev/sda4'
 
     @patch('suse_migration_services.logger.Logger.setup')
     @patch('os.path.ismount')
     def test_is_mounted(self, mock_os_path_ismount, mock_Logger_setup):
-        is_mounted('some')
+        self.mount_os.is_mounted('some')
         mock_os_path_ismount.assert_called_once_with('some')
 
     @patch('suse_migration_services.logger.Logger.setup')
-    @patch('suse_migration_services.units.mount_system.get_target_root')
+    @patch('suse_migration_services.units.mount_system.MountSystem.get_target_root')
     @patch('suse_migration_services.command.Command.run')
     @patch('suse_migration_services.units.mount_system.Fstab')
     @patch('os.path.isfile')
@@ -147,7 +155,7 @@ class TestMountSystem():
         mock_Fstab.return_value = fstab
         # fstab file found
         mock_os_path_isfile.return_value = True
-        assert read_system_fstab('some') == fstab
+        assert self.mount_os.read_system_fstab() == fstab
         mock_Command_run.assert_called_once_with(
             ['mount', mock_get_target_root.return_value, 'some']
         )
@@ -155,7 +163,7 @@ class TestMountSystem():
         mock_os_path_isfile.return_value = False
         mock_Command_run.reset_mock()
         with raises(DistMigrationSystemNotFoundException):
-            read_system_fstab('some')
+            self.mount_os.read_system_fstab()
         assert mock_Command_run.call_args_list == [
             call(['mount', mock_get_target_root.return_value, 'some']),
             call(['umount', 'some'], raise_on_error=False)
@@ -180,7 +188,7 @@ class TestMountSystem():
         fstab_mock.read.return_value = fstab.read('../data/fstab')
         fstab_mock.get_devices.return_value = fstab.get_devices()
 
-        mount_system('some', fstab_mock)
+        self.mount_os.mount_system(fstab_mock)
         assert mock_Command_run.call_args_list == [
             call(
                 [
@@ -225,4 +233,4 @@ class TestMountSystem():
         ]
         mock_Command_run.side_effect = Exception
         with raises(DistMigrationSystemMountException):
-            mount_system('some', fstab_mock)
+            self.mount_os.mount_system(fstab_mock)

suse_migration_services/defaults.py
@@ -136,8 +136,8 @@ class Defaults:
     def get_os_release():
         with open('/etc/os-release', 'r') as handle:
             valid_pairs = []
-            # Read the whole file, strip leading/trailing whitespace from the content,
-            # then split into lines.
+            # Read the whole file, strip leading/trailing whitespace
+            # from the content, then split into lines.
             raw_lines = handle.read().strip().split(os.linesep)
 
             for line_text in raw_lines:
@@ -151,10 +151,12 @@ class Defaults:
                 if len(parts) == 2:
                     key, value = parts
                     valid_pairs.append((key.lower(), value.strip('\'"')))
-                # else: malformed line (e.g., no '='), ignore it as per os-release behavior
+                # else: malformed line (e.g., no '='),
+                # ignore it as per os-release behavior
 
             if not valid_pairs:
-                # Handle cases where /etc/os-release is empty or contains only comments/invalid lines
+                # Handle cases where /etc/os-release is
+                # empty or contains only comments/invalid lines
                 return namedtuple('OSRelease', [])()
 
             keys, values = zip(*valid_pairs)
@@ -179,3 +181,38 @@ class Defaults:
                 [Defaults.get_system_root_path(), '/etc/default/grub']
             )
         )
+
+    @staticmethod
+    def update_env(preserve_info):
+        """
+        Update runtime environment with preserve_info dict
+        """
+        proxy_env = {}
+        for _, preserve_files in preserve_info.items():
+            if Defaults.get_proxy_path() in preserve_files:
+                with open(Defaults.get_proxy_path(), 'r') as proxy_file:
+                    for line in proxy_file.readlines():
+                        if not (line.startswith('#') or line.startswith(os.linesep)):
+                            # DMS currently takes http, https and ftp protocols
+                            # into consideration, so lower case them is ok
+                            key_value = line.lower(). \
+                                replace(os.linesep, ''). \
+                                replace('"', ''). \
+                                split('=')
+                            proxy_env.update(dict([key_value]))
+        if proxy_env.get('proxy_enabled') == 'yes':
+            del proxy_env['proxy_enabled']
+            os.environ.update(proxy_env)
+
+    @staticmethod
+    def log_env(log):
+        """
+        Provide information about the current environment.
+        """
+        log.info('Env variables')
+        env = ''
+        for key, value in sorted(os.environ.items()):
+            env += '{key}: {value}{newline}'.format(
+                key=key, value=value, newline=os.linesep
+            )
+        log.info(env)

commit 60bbf3d02e6e46bb44507a8916839d7c169d1b33

Fix mount_system
Fixes: 15549f619

suse_migration_services/units/mount_system.py
@@ -82,7 +82,7 @@ class MountSystem:
         self.activate_lvm()
 
         self.mount_system(
-            self.root_path, self.read_system_fstab(self.root_path)
+            self.read_system_fstab()
         )
 
         migration_config = MigrationConfig()

test/unit/units/mount_system_test.py
@@ -72,7 +72,7 @@ class TestMountSystem():
             ['mount', '-o', 'remount,rw', '/run/initramfs/isoscan']
         )
         mock_mount_system.assert_called_once_with(
-            '/system-root', mock_read_system_fstab.return_value
+            mock_read_system_fstab.return_value
         )
         mock_update_migration_config_file.assert_called_once_with()
         mock_activate_lvm.assert_called_once_with()