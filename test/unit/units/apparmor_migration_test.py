7af57345f9610389e5df76cec3620681ac33b6cd
Marcus Schäfer <marcus.schaefer@suse.com>
2026-05-11 12:49:54 +0200

    Fix missing tests for latest apparmor code change

--- test/unit/units/apparmor_migration_test.py
+++ test/unit/units/apparmor_migration_test.py
@@ -1,16 +1,25 @@
-from unittest.mock import patch
+from unittest.mock import patch, call
 from pytest import raises
 from tempfile import NamedTemporaryFile
 
 from suse_migration_services.units.apparmor_migration import main
 from suse_migration_services.exceptions import DistMigrationAppArmorMigrationException
+from suse_migration_services.drop_components import DropComponents
 
 
 @patch('suse_migration_services.logger.Logger.setup')
 class TestAppArmorMigration(object):
     @patch('suse_migration_services.zypper.Zypper.install')
     @patch('suse_migration_services.defaults.Defaults.get_grub_default_file')
-    def test_main(self, mock_get_grub_default_file, mock_Zypper_install, mock_logger_setup):
+    @patch.object(DropComponents, 'package_installed')
+    def test_main(
+        self,
+        mock_package_installed,
+        mock_get_grub_default_file,
+        mock_Zypper_install,
+        mock_logger_setup,
+    ):
+        mock_package_installed.return_value = True
         test_data = NamedTemporaryFile()
         with open(test_data.name, 'w') as f:
             f.write('GRUB_CMDLINE_LINUX_DEFAULT="splash=silent security=apparmor"')
@@ -18,12 +27,20 @@ class TestAppArmorMigration(object):
         main()
         with open(test_data.name) as f:
             assert f.read() == 'GRUB_CMDLINE_LINUX_DEFAULT="splash=silent security=selinux"'
-        mock_Zypper_install.assert_called_once_with(
-            'patterns-base-selinux',
-            extra_args=['--no-recommends'],
-            raise_on_error=False,
-            chroot='/system-root',
-        )
+        assert mock_Zypper_install.call_args_list == [
+            call(
+                'patterns-base-selinux',
+                extra_args=['--no-recommends'],
+                raise_on_error=False,
+                chroot='/system-root',
+            ),
+            call(
+                'venv-salt-minion',
+                extra_args=['--force'],
+                raise_on_error=False,
+                chroot='/system-root',
+            ),
+        ]
 
     @patch('fileinput.input')
     def test_main_raises(self, mock_fileinput, mock_logger_setup):
