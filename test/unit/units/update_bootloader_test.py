f4ec0c9e816bd522d16ecc1a2839cbe417497e27
paolodepa <paolo.depasquale@suse.com>
2026-09-24 10:37:14 +0200

    fix: only install shim on supported architectures

--- test/unit/units/update_bootloader_test.py
+++ test/unit/units/update_bootloader_test.py
@@ -8,13 +8,15 @@ class TestUpdateBootloader:
     def inject_fixtures(self, caplog):
         self._caplog = caplog
 
+    @patch('platform.machine')
     @patch('suse_migration_services.units.update_bootloader.MigrationConfig')
     @patch('suse_migration_services.logger.Logger.setup')
     @patch('suse_migration_services.zypper.Zypper.install')
     @patch('suse_migration_services.command.Command.run')
     def test_main(
-        self, mock_Command_run, mock_Zypper_install, mock_logger_setup, mock_MigrationConfig
+        self, mock_Command_run, mock_Zypper_install, mock_logger_setup, mock_MigrationConfig, mock_platform_machine
     ):
+        mock_platform_machine.return_value = 'x86_64'
         migration_config = Mock()
         migration_config.get_zypper_install_args.return_value = []
         mock_MigrationConfig.return_value = migration_config
@@ -24,5 +26,23 @@ class TestUpdateBootloader:
         )
         assert mock_Command_run.call_args_list == [
             call(['chroot', '/system-root', 'shim-install', '--removable'], raise_on_error=False),
+            call(['chroot', '/system-root', '/sbin/update-bootloader', '--reinit']),
+        ]
+
+    @patch('platform.machine')
+    @patch('suse_migration_services.units.update_bootloader.MigrationConfig')
+    @patch('suse_migration_services.logger.Logger.setup')
+    @patch('suse_migration_services.zypper.Zypper.install')
+    @patch('suse_migration_services.command.Command.run')
+    def test_main_ppc64le(
+        self, mock_Command_run, mock_Zypper_install, mock_logger_setup, mock_MigrationConfig, mock_platform_machine
+    ):
+        mock_platform_machine.return_value = 'ppc64le'
+        migration_config = Mock()
+        migration_config.get_zypper_install_args.return_value = []
+        mock_MigrationConfig.return_value = migration_config
+        main()
+        mock_Zypper_install.assert_not_called()
+        assert mock_Command_run.call_args_list == [
             call(['chroot', '/system-root', '/sbin/update-bootloader', '--reinit']),
         ]
